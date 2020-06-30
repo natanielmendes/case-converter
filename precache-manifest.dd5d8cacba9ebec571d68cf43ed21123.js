e7f3447c55fc15c6e6d393ffb84314187cd4e96c
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-06-30 18:43:01 -0300

    Updates

---
+++ precache-manifest.dd5d8cacba9ebec571d68cf43ed21123.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be7399ad7272f2828b5a28ff5ee7b6d",
+    "url": "/case-converter/index.html"
+  },
+  {
+    "revision": "4d81bfb7e5986a666888",
+    "url": "/case-converter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d365d7a6541c035c3aa",
+    "url": "/case-converter/static/js/2.25a2c37b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/case-converter/static/js/2.25a2c37b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d81bfb7e5986a666888",
+    "url": "/case-converter/static/js/main.91df2734.chunk.js"
+  },
+  {
+    "revision": "6476e362e7a45eaec349",
+    "url": "/case-converter/static/js/runtime-main.2b411092.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/case-converter/static/media/logo.5d5d9eef.svg"
+  }
+]);
